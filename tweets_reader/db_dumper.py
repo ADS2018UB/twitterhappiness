6d840ae9f8ca0fdfb405601e82212196e57ae93f
Alex Castrelo <neoares23@gmail.com>
2018-11-08 12:14:41 +0100

    Add function to dump a tweet in the database. Add test function
    The test function uses the get_tweets to get a tweet, and dumps it into the database

--- tweets_reader/db_dumper.py
+++ tweets_reader/db_dumper.py
@@ -1,11 +1,12 @@
 from pymongo import MongoClient
+from tweets_reader import get_tweets
 
 MLAB_CREDENTIALS_FILE = '../credentials/mlab_credentials.txt'
 
 
 class Dumper:
     def __init__(self):
-        # connect here
+        # make connection
         with open(MLAB_CREDENTIALS_FILE, 'r', encoding='utf-8') as f:
             [name, password, url, dbname] = f.read().splitlines()
 
@@ -17,7 +18,12 @@ class Dumper:
 
     def dump_tweet(self, tweet):
         # dump to the database
+        db = self.client.twitter_happiness
+        db.tweets.insert_one(tweet)
         pass
 
 
-d = Dumper()
+def test():
+    d = Dumper()
+    tweets = get_tweets([-75.3325, 40.0274, -71.6961, 41.4725], 5)
+    d.dump_tweet(tweets[0])
