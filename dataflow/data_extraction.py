b87991672bb4badb1483f60fa72c7d2c34e1a359
Alex Castrelo <neoares23@gmail.com>
2018-11-20 12:08:29 +0100

    Add connection to the twitter API. Modify class structure.

--- dataflow/data_extraction.py
+++ dataflow/data_extraction.py
@@ -5,10 +5,11 @@
 
 
 class DataExtraction:
-    def __init__(self, location):
-        self.location = location
+    def __init__(self, twitter_api):
+        self.twitter_api = twitter_api
         pass
 
     def collect(self, location):
         print("Collecting data for", location["name"])
+
         return "some data"
--- dataflow/main_daily.py
+++ dataflow/main_daily.py
@@ -7,9 +7,11 @@ import db_connection
 from data_extraction import DataExtraction
 import data_transformation
 import data_load
+import tweepy
 
 
 DB_CREDENTIALS = "../credentials/mlab_credentials.txt"
+TWITTER_CREDENTIALS = '../credentials/twitter_credentials.txt'
 DB_LOCATIONS_COLLECTION = "twitter_happiness_locations"
 
 
@@ -17,12 +19,23 @@ if __name__ == '__main__':
 
     db_locations = db_connection.connect(DB_CREDENTIALS, DB_LOCATIONS_COLLECTION)
 
+    with open(TWITTER_CREDENTIALS, 'r', encoding='utf-8') as f:
+        [access_key, access_secret, consumer_key, consumer_secret] = f.read().splitlines()
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+    twitter_api = tweepy.API(auth)
+    print("twitter connection successful")
+
+    de = DataExtraction(twitter_api)
+
+
+
     print()
     for location in db_locations.find():
         print("Processing data for ", location["name"])
 
         # extraction: download tweets from Twitter API
-        de = DataExtraction(location['box'])
         data = de.collect(location)
 
         # transformation: process/analize downloaded tweets
