1beb5d63080dd2d10954670a5d3bd5686fc98102
Alex Castrelo <neoares23@gmail.com>
2018-11-20 13:08:40 +0100

    Get tweets based on the location

--- dataflow/data_extraction.py
+++ dataflow/data_extraction.py
@@ -12,4 +12,11 @@ class DataExtraction:
     def collect(self, location):
         print("Collecting data for", location["name"])
 
-        return "some data"
+        places = self.twitter_api.reverse_geocode(
+            lat=location['lat_center'],
+            long=location['lon_center'],
+            accuracy=location['radius'])
+        place_id = places[0].id
+        tweets = self.twitter_api.search(q="place:%s" % place_id, lang='en', count=10)
+
+        return tweets
