ce99de03a6f07e7bd13f4410716571cc9378df4c
gmarrugat <gmarrugat@gmail.com>
2018-12-05 12:28:54 +0100

    remove test files

--- dataflow/date_format.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-
-def date_format(data):
-
-    for elem in data:
-        date_str = elem['created_at']
-        date_obj = datetime.datetime.strptime(date_str, '%a %b %d %H:%M:%S %z %Y')
-        elem['datetime'] = datetime.datetime.combine(date_obj.date(), date_obj.time())
